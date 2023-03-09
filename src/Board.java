f2777d5e56b2ec54720bb1e960e1e2444889bbe7
fe-i <feic47@gmail.com>
2023-03-08 19:05:26 -0500

    files

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,82 @@
-package PACKAGE_NAME;public class Board {
+public class Board {
+    private int[][] board;
+    private String name;
+    private final int BOARD_SIZE = 5; // Board Size
+
+    public Board(String name) {
+        this.name = name;
+        board = new int[BOARD_SIZE][BOARD_SIZE];
+    }
+
+    public boolean placeNum(String letter, int newVal) {
+        if (letter.length() > 0) letter = letter.substring(0, 1);
+        else return false;
+
+        int pos = getIdxFromLetter(letter);
+        if (pos < 0 || pos > Math.pow(BOARD_SIZE, 2) - 1) return false;
+        int r = pos / BOARD_SIZE;
+        int c = pos % BOARD_SIZE;
+        if (board[r][c] == 0) {
+            board[r][c] = newVal;
+            return true;
+        }
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getLetterFromIdx(int idx) {
+        return String.valueOf((char)(idx + 65));
+    }
+
+    public int getIdxFromLetter(String letter) {
+        return letter.charAt(0) - 65;
+    }
+
+    public void printBoard() {
+        System.out.println(name + "'s Board:");
+        System.out.println("---------------------------");
+
+        for (int r = 0; r < BOARD_SIZE; r++) {
+            System.out.print("| ");
+            for (int c = 0; c < BOARD_SIZE; c++) {
+                int tile = board[r][c];
+                if (tile != 0) System.out.print(tile + (tile < 10 ? " " : "") + " | ");
+                else System.out.print(Colors.BLUE + getLetterFromIdx(r * BOARD_SIZE + c) + Colors.RESET + "  | ");
+            }
+            System.out.println("\n---------------------------");
+        }
+    }
+
+    public int getScore() {
+        int sum = 0;
+
+        for (int r = 0; r < BOARD_SIZE; r++) {
+            int streak = 1;
+            for (int c = 1; c < BOARD_SIZE; c++) {
+                if (board[r][c - 1] == board[r][c]) {
+                    streak++;
+                } else {
+                    if (streak > 1) sum += board[r][c - 1] * streak;
+                    streak = 1;
+                }
+            }
+            if (streak > 1) sum += board[r][BOARD_SIZE - 1] * streak;
+        }
+
+        for (int c = 0; c < BOARD_SIZE; c++) {
+            int streak = 1;
+            for (int r = 1; r < BOARD_SIZE; r++) {
+                if (board[r - 1][c] == board[r][c]) {
+                    streak++;
+                } else {
+                    if (streak > 1) sum += board[r - 1][c] * streak;
+                    streak = 1;
+                }
+            }
+            if (streak > 1) sum += board[BOARD_SIZE - 1][c] * streak;
+        }
+        return sum;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,63 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to Neighbors!");
+        System.out.print("Player 1, please enter your name: ");
+        String p1Name = scan.nextLine();
+
+        System.out.println();
+
+        System.out.print("Player 2, please enter your name: ");
+        String p2Name = scan.nextLine();
+
+        System.out.println();
+
+        Board[] players = {new Board(p1Name), new Board(p2Name)};
+
+        int turn = 0;
+        int randomNumber = (int) (Math.random() * 10) + 1;
+        do {
+            players[turn % 2].printBoard();
+            System.out.println(Colors.PURPLE + "The number is: " + Colors.RESET + Colors.CYAN + randomNumber + Colors.RESET);
+            System.out.print(Colors.YELLOW + "Enter your move as a letter: " + Colors.RESET);
+            String letter = scan.nextLine().toUpperCase();
+            boolean placed = players[turn % 2].placeNum(letter, randomNumber);
+
+            if (placed) {
+                turn++;
+                if (turn % 2 == 0) randomNumber = (int) (Math.random() * 10) + 1;
+            }
+            else {
+                System.out.println(Colors.RED + "Invalid move!" + Colors.RESET);
+            }
+        } while (turn < 2 * Math.pow(5 /*Board Size*/, 2));
+
+        System.out.println();
+
+        Board[] results = new Board[2];
+        if (players[0].getScore() > players[1].getScore()) {
+            results[0] = players[0];
+            results[1] = players[1];
+        }
+        else if (players[1].getScore() > players[0].getScore()) {
+            results[0] = players[1];
+            results[1] = players[0];
+        }
+        else {
+            for (Board b: players) {
+                b.printBoard();
+            }
+            System.out.println("The game was somehow a tie!???");
+            System.exit(0);
+        }
+
+        for (Board b: players) {
+            b.printBoard();
+        }
+        System.out.println(Colors.GREEN + results[0].getName() + " has won the game, achieving a score of " + results[0].getScore() + Colors.RESET);
+        System.out.println(Colors.RED + results[1].getName() + " lost by " + (results[0].getScore() - results[1].getScore()) + " points! ;-;" + Colors.RESET);
     }
 }
